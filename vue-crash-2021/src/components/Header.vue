6108fba1406df604e83ec8b595847dbd610d121f
Fossil <colewestbrookphoto@gmail.com>
2021-10-08 13:31:35 -0400

    First task app progress

---
+++ vue-crash-2021/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: "Header",
+  // Just importing an array of prop pointers to look for
+  // props: ['title'],
+
+  props: {
+    // Define type
+    // title: String,
+    title: {
+      type: String,
+      default: 'Hello World'
+    },
+  },
+  components: {
+    // List imported components?
+    Button,
+  }
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
